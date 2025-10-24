889fddfa37756ac42af89fb81207f7751f046c69
Roguree <touko1996@gmail.com>
2025-10-24 10:59:51 +0900

    feature / basic monster animation commit

--- Assets/02. Scripts/Monster/MonsterAnimation.cs
+++ Assets/02. Scripts/Monster/MonsterAnimation.cs
@@ -2,28 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour
+public class MonsterAnimation : MonoBehaviour
 {
     [Header("설정")]
     [SerializeField] private float scaleAmount = 0.05f; //늘어나는 정도
     [SerializeField] private float speed = 8f; // 숨쉬는 속도
-    [SerializeField] private bool isRunning = false; //기본을 false로 두고 달릴 때 true로 전환
 
     private Vector3 originScale;
 
     private void Start()
     {
         originScale = transform.localScale;
-       
+
     }
     private void Update()
     {
-        float currentSpeed = isRunning ? speed * 2f : speed;
-        float newY = originScale.y + Mathf.Sin(Time.time * currentSpeed)*scaleAmount;
+        
+        float newY = originScale.y + Mathf.Sin(Time.time * speed) * scaleAmount;
         transform.localScale = new Vector3(originScale.x, newY, originScale.z);
     }
-    public void SetRunning(bool running)
-    {
-        isRunning = running;
-    }
+    
 }
